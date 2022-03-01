5f1b201173a0619aecd2f8c780b2e312bfdea3a1
AnetaCadova <aneta.cadova@gmail.com>
2022-03-01 14:28:54 +0100

    update 7.10.0 version to 7.10.1

--- src/test/java/apicurito/tests/configuration/ReleaseSpecificParameters.java
+++ src/test/java/apicurito/tests/configuration/ReleaseSpecificParameters.java
@@ -2,18 +2,21 @@ package apicurito.tests.configuration;
 
 public class ReleaseSpecificParameters {
     public static final String APICURITO_IMAGE_VERSION = "1.10";
-    public static final String APICURITO_CURRENT_VERSION = "7.10.0";
-    public static final String APICURITO_PREVIOUS_VERSION = "7.9";
+    public static final String APICURITO_CURRENT_VERSION = "7.10.1";
+    public static final String APICURITO_PREVIOUS_VERSION = "7.10.0";
     public static final String APICURITO_TEMPLATE_URL =
         "https://raw.githubusercontent.com/jboss-fuse/application-templates/2.1.x.sb2.redhat-7-x/fuse-apicurito.yml";
     public static final String APICURITO_IS_TEMPLATE_URL =
         "https://raw.githubusercontent.com/jboss-fuse/application-templates/2.1.x.sb2.redhat-7-x/fis-image-streams.json";
-    public static final String OLD_OPERATOR_URL = "registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-apicurito-rhel8-operator:1.9-15";
+    public static final String OLD_OPERATOR_URL = "registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-apicurito-rhel8-operator:1.10-9";
     //GA image
     public static final String APICURITO_OPERATOR_7_7_METADATA_URL =
         "registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-apicurito-operator-metadata:1.7";
     public static final String APICURITO_OPERATOR_7_8_METADATA_URL =
         "registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-apicurito-operator-metadata:1.8";
     public static final String APICURITO_OPERATOR_7_9_METADATA_URL = "registry.redhat.io/fuse7/fuse-apicurito-rhel8-operator-bundle:1.9";
+    // GA image
+    public static final String APICURITO_OPERATOR_7_10_0_METADATA_URL =
+        "registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-apicurito-rhel8-operator-metadata:1.10-16";
     // GA image
 }
