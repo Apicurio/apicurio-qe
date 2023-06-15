dc851fbab97cb522fcaf2661cb38a5007ab46e15
mkralik3 <mkralik@redhat.com>
2023-06-15 10:33:22 +0200

    Bump to 7.12

--- src/test/java/apicurito/tests/configuration/ReleaseSpecificParameters.java
+++ src/test/java/apicurito/tests/configuration/ReleaseSpecificParameters.java
@@ -1,18 +1,18 @@
 package apicurito.tests.configuration;
 
 public class ReleaseSpecificParameters {
-    public static final String APICURITO_IMAGE_VERSION = "1.11";
-    public static final String APICURITO_CURRENT_VERSION = "7.11";
-    public static final String APICURITO_PREVIOUS_VERSION = "7.10.1";
+    public static final String APICURITO_IMAGE_VERSION = "1.12";
+    public static final String APICURITO_CURRENT_VERSION = "7.12";
+    public static final String APICURITO_PREVIOUS_VERSION = "7.11";
     public static final String APICURITO_TEMPLATE_URL =
         "https://raw.githubusercontent.com/jboss-fuse/application-templates/2.1.x.sb2.redhat-7-x/fuse-apicurito.yml";
     public static final String APICURITO_IS_TEMPLATE_URL =
         "https://raw.githubusercontent.com/jboss-fuse/application-templates/2.1.x.sb2.redhat-7-x/fis-image-streams.json";
-    public static final String OLD_OPERATOR_URL = "registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-apicurito-rhel8-operator:1.10";
+    public static final String OLD_OPERATOR_URL = "registry-proxy.engineering.redhat.com/rh-osbs/fuse7-fuse-apicurito-rhel8-operator:1.11";
 
-    public static final String APICURITO_CURRENT_UPDATE_CHANNEL = "fuse-apicurito-7.11.x";
-    public static final String APICURITO_OLD_UPDATE_CHANNEL = "fuse-apicurito-7.10.x";
+    public static final String APICURITO_CURRENT_UPDATE_CHANNEL = "fuse-apicurito-7.12.x";
+    public static final String APICURITO_OLD_UPDATE_CHANNEL = "fuse-apicurito-7.11.x";
 
-    public static final String APICURITO_CURRENT_CSV = "fuse-apicurito.v7.11.0";
-    public static final String APICURITO_OLD_CSV = "fuse-apicurito.v7.10.3";
+    public static final String APICURITO_CURRENT_CSV = "fuse-apicurito.v7.12.0";
+    public static final String APICURITO_OLD_CSV = "fuse-apicurito.v7.11.1";
 }
